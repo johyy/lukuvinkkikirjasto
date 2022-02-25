bf85edbaee24d0e512aa7e4a1c1d8c18613e82ee
Peruna <peruna@iki.fi>
2022-02-25 11:34:35 +0200

    service-pohjia recom

---
+++ src/services/recommendation_service.py
@@ -0,0 +1,10 @@
+class RecommendationService:
+    """ Class responsible for media logic."""
+
+    def __init__():
+        """ Class constructor. Creates a new media service.
+        Args:"""
+
+
+ 
+resommendation_service = RecommendationService()
