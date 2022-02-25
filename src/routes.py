b7decf423a573897dbaf58cd2754010154e771aa
katja.kerkkanen@helsinki.fi <katjaker@cubbli20-gold.local>
2022-02-25 11:53:11 +0200

    routes and templates updated

--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,25 @@
 from app import app
 from flask import render_template, request, redirect
+from services.user_service import UserService
+from services.recommendation_service import RecommendationService
+
 
 
 @app.route("/")
 def index():
-
     return render_template("index.html")
+
+
+@app.route("/login", methods=["get", "post"])
+def login():
+    pass
+
+
+@app.route("/logout")
+def logout():
+    pass
+
+
+@app.route("/sign_up", methods=["get", "post"])
+def sign_up():
+    pass
