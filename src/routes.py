00f6f4d4e235c3361daf6df6e51d4852626d90bb
kerkkanen <katjaker@cubbli20-gold.local>
2022-02-28 13:23:47 +0200

    routes.py updated to macth app_service.py

--- src/routes.py
+++ src/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, request, redirect, url_for
 from services.user_service import UserService as user_service
 from services.recommendation_service import RecommendationService as recommendation_service
+from services.app_service import AppService as app_service
 from repositories.tip_repository import TipRepository as tip_repository
 
 
@@ -11,15 +12,18 @@ def index():
 
     return render_template("index.html", sort_option="1", recommendations_list=recommendations_list)
 
-@app.route("/sort_by", methods = ["POST"])
+
+@app.route("/sort_by", methods=["POST"])
 def sort_by():
     sort_option = request.form["sort_option"]
     return redirect(url_for("index_sorted", sort_option=sort_option))
 
+
 @app.route("/<sort_option>")
 def index_sorted(sort_option):
 
-    recommendations_list = tip_repository.fetch_all_tips(tip_repository, sort_option)
+    recommendations_list = tip_repository.fetch_all_tips(
+        tip_repository, sort_option)
     return render_template("index.html", sort_option=sort_option, recommendations_list=recommendations_list)
 
 
@@ -32,13 +36,7 @@ def login():
         name = request.form["name"]
         password = request.form["password"]
 
-        valid, error = user_service.login(name, password)
-        # Erilaiset error-viestit luotava user_servicessä,
-        # jolloin oikean viestin voi palauttaa suoraan kyseisestä virheestä
-        # (esim. puuttuuko jostain kentästä jotain,
-        # , onko salasana väärä tms.
-        # validate palauttaa siis joko True, "" tai False, "*virheen syy*")
-
+        valid, error = app_service.login(name, password)
         if not valid:
             return render_template("login.html", error=error)
         return redirect("/")
@@ -46,7 +44,7 @@ def login():
 
 @app.route("/logout")
 def logout():
-    user_service.logout()
+    app_service.logout()
     return redirect("/")
 
 
@@ -61,18 +59,11 @@ def register():
         password = request.form["password"]
         password_again = request.form["password_again"]
 
-        valid, error = user_service.validate(name, password, password_again)
+        valid, error = app_service.validate(name, password, password_again)
         if not valid:
-            return render_template("sign_up.html", error=error)
+            return render_template("register.html", error=error)
 
-        # Erilaiset error-viestit luotava user_servicessä,
-        # jolloin oikean viestin voi palauttaa suoraan kyseisestä virheestä
-        # (esim. puuttuuko jostain kentästä jotain,
-        # , onko nimimerkki varattu tai salasana epäkelpo tms.
-        # validate palauttaa siis joko True, "" tai False, "*virheen syy*")
-
-        valid, error = user_service.register(name, password)
+        valid, error = app_service.register(name, password)
         if valid:
             return redirect("/")
         return render_template("register.html", error=error)
-
