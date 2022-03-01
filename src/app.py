8a914d219eef88bdbe1b69bdffa8f57499e4aca4
TuuPu <puontit@gmail.com>
2022-03-01 13:09:46 +0200

    Removed =getenv(DATABASE_URL) from app.py still

--- src/app.py
+++ src/app.py
@@ -5,8 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///nomanoma"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tuukkapuonti"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 db = SQLAlchemy(app)
