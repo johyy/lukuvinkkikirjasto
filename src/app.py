48b9a6cd8601bdf211fd63049d5049b64d94112e
kerkkanen <katja.kerkkanen@helsinki.fi>
2022-03-01 13:47:30 +0200

    merge update

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tuukkapuonti"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///katjaker"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
@@ -14,3 +14,5 @@ from entities.recommendation import Recommendation
 
 db.create_all()
 db.session.commit()
+
+import routes
