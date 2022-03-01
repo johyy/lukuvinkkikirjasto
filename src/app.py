2a4f98143a2108200e81d4f19d6d07e58293a1a8
TuuPu <puontit@gmail.com>
2022-03-01 13:16:58 +0200

    Finally fixed app.py and repositories so that they actually work and tests go through

--- src/app.py
+++ src/app.py
@@ -1,16 +1,14 @@
 from os import getenv
 from flask import Flask
-from db import db
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tuukkapuonti"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
 db = SQLAlchemy(app)
 
-import routes
+
 from entities.user import User_account
 from entities.recommendation import Recommendation
 
