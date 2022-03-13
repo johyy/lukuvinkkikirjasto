77651ae215af77b298dbd1db9ee0582dd09ae6c5
limi96 <mikelee199607@gmail.com>
2022-03-13 23:02:41 +0200

    Changes to tests: Renamed index_listing.robot and unit tests

--- src/tests/test_recommendation_repository.py
+++ src/tests/test_recommendation_repository.py
@@ -5,17 +5,21 @@ from entities.recommendation import Recommendation
 from entities.user import UserAccount
 
 
-
 class TestRecommendationRepository(unittest.TestCase):
 
     def setUp(self):
         recommendation_repository.delete_all_recommendations()
         recommendation_repository.delete_all_likes()
         user_repository.delete_all()
-        self.test_recom = Recommendation(
-            title="Harry Potter", link="http://www.harrypotter.com", user_id=1)
+
+        recom_one = (Recommendation(title="one", link="https://linkki", user_id=1))
+        recom_two = (Recommendation(title="two", link="https://linkki", user_id=1))
+        recom_three = (Recommendation(title="three", link="https://linkki", user_id=1))
+        recommendation_repository.add_new_recommendation(recom_one)
+        recommendation_repository.add_new_recommendation(recom_two)
+        recommendation_repository.add_new_recommendation(recom_three)
+
         self.test_user = UserAccount("testi", "kayttaja123")
-        recommendation_repository.add_new_recommendation(self.test_recom)
         user_repository.add_a_new_user(self.test_user)
 
     def test_add_recommendation(self):
@@ -23,77 +27,55 @@ class TestRecommendationRepository(unittest.TestCase):
             title="Kirja", link="https://linkki", user_id=1))
         recommendation_repository.add_new_recommendation(test_recom)
         fetch_all = recommendation_repository.fetch_all_recommendations()
-        self.assertEqual("Kirja", fetch_all[1][1])
+        self.assertEqual("Kirja", fetch_all[3][1])
 
     def test_add_existing_recommendation(self):
+        recommendation_repository.delete_all_recommendations()
+
+        test_recom = Recommendation(
+            title="Harry Potter", link="http://www.harrypotter.com", user_id=1)
+        recommendation_repository.add_new_recommendation(test_recom)
         test_recom = (Recommendation(
             title="Harry Potter", link="https://linkki", user_id=1))
         recommendation_repository.add_new_recommendation(test_recom)
+
         fetch_all = recommendation_repository.fetch_all_recommendations()
         self.assertEqual(1, len(fetch_all))
 
-    def test_fetch_first(self):
-        test_recom = (Recommendation(
-            title="Kirja", link="https://linkki", user_id=1))
-        recommendation_repository.add_new_recommendation(test_recom)
-        fetch_all = recommendation_repository.fetch_all_recommendations()
-        self.assertEqual("Kirja", fetch_all[1][1])
-        recommendation_repository.delete_recommendation(2)
-
     def test_fetch_multiple_order_default(self):
-        recom_one = (Recommendation(title="one", link="https://linkki", user_id=1))
-        recom_two = (Recommendation(title="two", link="https://linkki", user_id=1))
-        recom_three = (Recommendation(title="three", link="https://linkki", user_id=1))
-        recommendation_repository.add_new_recommendation(recom_one)
-        recommendation_repository.add_new_recommendation(recom_two)
-        recommendation_repository.add_new_recommendation(recom_three)
-
         fetch_all_default = recommendation_repository.fetch_all_recommendations()
-        self.assertEqual("one", fetch_all_default[1][1])
-        self.assertEqual("two", fetch_all_default[2][1])
-        self.assertEqual("three", fetch_all_default[3][1])
+        self.assertEqual("one", fetch_all_default[0][1])
+        self.assertEqual("two", fetch_all_default[1][1])
+        self.assertEqual("three", fetch_all_default[2][1])
 
         fetch_all = recommendation_repository.fetch_all_recommendations(
             sort_option="1")
         self.assertEqual(fetch_all, fetch_all_default)
 
-    def test_fetch_multiple_order_likes(self):
-        recom_one = (Recommendation(title="one", link="https://linkki", user_id=1))
-        recom_two = (Recommendation(title="two", link="https://linkki", user_id=1))
-        recom_three = (Recommendation(title="three", link="https://linkki", user_id=1))
-        recommendation_repository.add_new_recommendation(recom_one)
-        recommendation_repository.add_new_recommendation(recom_two)
-        recommendation_repository.add_new_recommendation(recom_three)
-
+    def test_fetch_multiple_order_by_likes(self):
         recommendation_repository.add_like(3, 2)
         fetch_all = recommendation_repository.fetch_all_recommendations(
             sort_option="2")
         print(fetch_all)
         self.assertEqual("three", fetch_all[2][1])
-        self.assertEqual("one", fetch_all[1][1])
+        self.assertEqual("one", fetch_all[0][1])
 
-        recommendation_repository.add_like(3, 6)
+        recommendation_repository.add_like(2, 6)
 
         fetch_all = recommendation_repository.fetch_all_recommendations()
 
         self.assertEqual("two", fetch_all[0][1])
 
-    def test_fetch_multiple_order_time(self):
-        recom_one = (Recommendation(title="one", link="https://linkki", user_id=1))
-        recom_two = (Recommendation(title="two", link="https://linkki", user_id=1))
-        recom_three = (Recommendation(title="three", link="https://linkki", user_id=1))
-        recommendation_repository.add_new_recommendation(recom_one)
-        recommendation_repository.add_new_recommendation(recom_two)
-        recommendation_repository.add_new_recommendation(recom_three)
+    def test_fetch_multiple_order_by_time(self):
         fetch_all_newest = recommendation_repository.fetch_all_recommendations(
             sort_option="3")
-        self.assertEqual("three", fetch_all_newest[3][1])
-        self.assertEqual("one", fetch_all_newest[1][1])
+        self.assertEqual("three", fetch_all_newest[2][1])
+        self.assertEqual("one", fetch_all_newest[0][1])
 
         fetch_all_oldest = recommendation_repository.fetch_all_recommendations(
             sort_option="4")
-        self.assertEqual("three", fetch_all_oldest[3][1])
-        self.assertEqual("one", fetch_all_oldest[1][1])
+        self.assertEqual("three", fetch_all_oldest[2][1])
+        self.assertEqual("one", fetch_all_oldest[0][1])
 
     def test_test_like_to_add_true(self):
         self.assertTrue(recommendation_repository.test_like_to_add(10, 10))
@@ -118,12 +100,26 @@ class TestRecommendationRepository(unittest.TestCase):
     def test_fetch_recommendations_liked_by_user_no_likes(self):
         self.assertEqual(recommendation_repository.fetch_recommendations_liked_by_user(1), [])
 
+    def test_delete_all_recommendations(self): 
+        recommendation_repository.delete_all_recommendations()
+        self.assertEqual([], recommendation_repository.fetch_all_recommendations())
+
     def test_delete_recommendation(self):
+        recommendation_repository.delete_all_recommendations()
+        
+        test_recom = (Recommendation(
+            title="Kirja", link="https://linkki", user_id=1))
+        recommendation_repository.add_new_recommendation(test_recom)
+
         recommendation_repository.delete_recommendation(1)
         fetch_all = recommendation_repository.fetch_all_recommendations()
         self.assertEqual([], fetch_all)
 
     def test_get_recommendation_by_user_id(self):
+        recommendation_repository.delete_all_recommendations()
+        test_recom = Recommendation(
+            title="Harry Potter", link="http://www.harrypotter.com", user_id=1)
+        recommendation_repository.add_new_recommendation(test_recom)
         test_recom = Recommendation(
             title="Porri Hatter", link="http://www.porrihatter.com", user_id=2)
         recommendation_repository.add_new_recommendation(test_recom)
